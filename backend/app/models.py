3bd0d6825eb0cc35a4d406b7e75b0be5a1a61e39
Ottohesten <otto@skytop.dk>
2025-02-04 01:44:15 +0100

    changed recipe description field to instructions field

--- backend/app/models.py
+++ backend/app/models.py
@@ -170,7 +170,7 @@ class RecipeIngredientLink(SQLModel, table=True):
 
 class RecipeBase(SQLModel):
     title: str = Field(max_length=255, min_length=1)
-    description: str | None = Field(default=None, max_length=255)
+    instructions: str | None = Field(default=None, max_length=255)
 
 
 class RecipeCreate(RecipeBase):
@@ -186,7 +186,7 @@ class Recipe(RecipeBase, table=True):
     """
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     title: str = Field(max_length=255)
-    description: str | None = Field(default=None, max_length=255)
+    instrutions: str | None = Field(default=None, max_length=255)
 
     owner_id: uuid.UUID = Field(foreign_key="user.id", nullable=False)
     owner: User = Relationship(back_populates="recipes")

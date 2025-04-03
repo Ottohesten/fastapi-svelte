7f7e2cec527a41f83fd18671f94b4d6ce27f9a78
Ottohesten <otto@skytop.dk>
2025-04-03 23:25:33 +0200

    logging the url of the response on heroes

--- frontend/src/routes/hero/+layout.server.js
+++ frontend/src/routes/hero/+layout.server.js
@@ -5,6 +5,9 @@ export const load = async ({ fetch, locals }) => {
     const client = createApiClient(fetch);
     const { data, error: apierror, response } = await client.GET("/heroes/");
 
+    // log the url of the request
+    console.log("response url: ", response.url);
+
     if (apierror) {
         error(404, apierror);
     }

5d92ba564fa6e04ba357502103852b1f2427deca
Ottohesten <otto@skytop.dk>
2025-04-04 01:57:45 +0200

    fixed issue with authed endpoints because user locals was changed

--- frontend/src/routes/(authed)/+layout.server.ts
+++ frontend/src/routes/(authed)/+layout.server.ts
@@ -1,8 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 
 export function load({ locals, url }) {
-    const { user } = locals;
-    if (!user?.is_superuser) {
+    const { authenticatedUser } = locals;
+    if (!authenticatedUser?.is_superuser) {
         redirect(303, `/auth/login?redirectTo=${url.pathname}`);
     }
 

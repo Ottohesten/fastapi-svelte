7766a9e0d54059edc1a8385b2b2289fb28ef95e0
Ottohesten <otto@skytop.dk>
2025-04-04 01:38:05 +0200

    chaning back hero to normal

--- frontend/src/routes/hero/+layout.server.js
+++ frontend/src/routes/hero/+layout.server.js
@@ -2,22 +2,22 @@ import { createApiClient } from '$lib/api/api';
 import { error } from '@sveltejs/kit';
 
 export const load = async ({ fetch, locals }) => {
-    // const client = createApiClient(fetch);
-    // console.log("making request")
-    // const { data, error: apierror, response } = await client.GET("/heroes/");
+    const client = createApiClient(fetch);
+    console.log("making request")
+    const { data, error: apierror, response } = await client.GET("/heroes/");
 
-    // // log the url of the request
-    // console.log("response url: ", response.url);
+    // log the url of the request
+    console.log("response url: ", response.url);
 
-    // if (apierror) {
-    //     error(404, apierror);
-    // }
-    // // console.log(data)
+    if (apierror) {
+        error(404, apierror);
+    }
+    // console.log(data)
 
 
-    const response = await fetch('https://fastapi-svelte.onrender.com/heroes/');
-    const data = await response.json();
-    console.log(data)
+    // const response = await fetch('https://fastapi-svelte.onrender.com/heroes/');
+    // const data = await response.json();
+    // console.log(data)
 
 
 

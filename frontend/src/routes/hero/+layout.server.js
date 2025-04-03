cde9772a0009f75a2d83c0112f128336d6201931
Ottohesten <otto@skytop.dk>
2025-04-04 01:14:44 +0200

    changed heroes endpoint to use vanilla fetch with hardcoded url for testing purposes

--- frontend/src/routes/hero/+layout.server.js
+++ frontend/src/routes/hero/+layout.server.js
@@ -2,22 +2,26 @@ import { createApiClient } from '$lib/api/api';
 import { error } from '@sveltejs/kit';
 
 export const load = async ({ fetch, locals }) => {
-    const client = createApiClient(fetch);
-    console.log("making request")
-    const { data, error: apierror, response } = await client.GET("/heroes/");
+    // const client = createApiClient(fetch);
+    // console.log("making request")
+    // const { data, error: apierror, response } = await client.GET("/heroes/");
 
-    // log the url of the request
-    console.log("response url: ", response.url);
+    // // log the url of the request
+    // console.log("response url: ", response.url);
 
-    if (apierror) {
-        error(404, apierror);
-    }
-    // console.log(data)
+    // if (apierror) {
+    //     error(404, apierror);
+    // }
+    // // console.log(data)
+
+
+    const response = await fetch('https://fastapi-svelte.onrender.com/heroes/');
+    console.log(response);
 
 
 
 
     return {
-        heroes: data
+        // heroes: data
     }
 }

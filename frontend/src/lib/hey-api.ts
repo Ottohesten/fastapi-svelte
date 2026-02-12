05b75edc350c62a27a59a4ee119d035f8bdd7dbb
Otto Schmidt <otto@skytop.dk>
2026-02-12 03:47:55 +0100

    removed commented out code

--- frontend/src/lib/hey-api.ts
+++ frontend/src/lib/hey-api.ts
@@ -3,5 +3,11 @@ import { env } from "$env/dynamic/private";
 
 export const createClientConfig: CreateClientConfig = (config) => ({
     ...config,
+    // auth: () => "auth_token",
     baseUrl: env.BACKEND_HOST || "http://127.0.0.1:8000"
+
+    // headers: {
+    //     ...config?.headers,
+    //     "Authorization": "Bearer "
+    // }
 });
